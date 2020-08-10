2d6772c96817a004ddf413fc20edef621ea3f7a9
kiziri <58429073+kiziri@users.noreply.github.com>
2020-08-10 14:36:04 +0900

    Update MainActivity.java
    Add click event method for start page view

--- app/src/main/java/com/example/chat_application/MainActivity.java
+++ app/src/main/java/com/example/chat_application/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.chat_application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void mainPageCLick(View view) {
+        switch (view.getId()) {
+            case R.id.userLoginBtn :
+
+                break;
+            case R.id.userJoinBtn :
+
+                break;
+        }
+    }   
 }
