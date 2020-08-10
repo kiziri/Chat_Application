8c437a5d5681c8dc4884119dc580779373988d89
kiziri <58429073+kiziri@users.noreply.github.com>
2020-08-10 16:22:51 +0900

    Update MainActivity.java
    Add method for get Debug HashKey

--- app/src/main/java/com/example/chat_application/MainActivity.java
+++ app/src/main/java/com/example/chat_application/MainActivity.java
@@ -3,8 +3,15 @@ package com.example.chat_application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
+
+import java.security.MessageDigest;
 
 public class MainActivity extends AppCompatActivity {
     Intent intent;
@@ -12,6 +19,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 디버깅 해시키 메소드 호출
+        getAppKeyHash();
+    }
+
+    // 디버깅용 해시키 가져오는 메소드
+    private void getAppKeyHash() {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md;
+                md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String something = new String(Base64.encode(md.digest(), 0));
+                Log.e("Hash key", something);
+            }
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            Log.e("name not found", e.toString());
+        }
     }
 
     public void mainPageCLick(View view) {
