dcf5d0bc84d8dbe30c763b2e541857aee427ef9e
kiziri <58429073+kiziri@users.noreply.github.com>
2020-08-10 14:44:08 +0900

    Update MainActivity.java
    Add click event for login button
    make the event to change to login page view

--- app/src/main/java/com/example/chat_application/MainActivity.java
+++ app/src/main/java/com/example/chat_application/MainActivity.java
@@ -2,11 +2,12 @@ package com.example.chat_application;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,7 +17,9 @@ public class MainActivity extends AppCompatActivity {
     public void mainPageCLick(View view) {
         switch (view.getId()) {
             case R.id.userLoginBtn :
-
+                intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
                 break;
             case R.id.userJoinBtn :
 
